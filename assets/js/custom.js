c11e204f988c2780d5bce3d8a183ed4c77d67dc1
WangZhen88 <753765551@qq.com>
2017-03-27 16:39:36 +0800

    我的简历

---
+++ assets/js/custom.js
@@ -0,0 +1,62 @@
+﻿
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+
+            $(function () {
+                $('.move-me a').bind('click', function (event) { //just pass move-me in design and start scrolling
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+
+
+            $(function () {
+                $('.chart').easyPieChart({
+                    easing: 'easeOutBounce',
+                    onStep: function (from, to, percent) {
+                        $(this.el).find('.percent').text(Math.round(percent));
+                    },
+                    barColor: '#05D6AC',
+                    lineWidth: 10,
+                    size: 150
+                });
+
+            });
+
+            $(function () {
+                $('a[title]').tooltip();
+            });
+
+            $('#testimonial-main').carousel({
+                interval: 4000
+            });
+
+
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+            new WOW().init();
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    };
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
---
+++ assets/js/styleSwitcher.js
@@ -0,0 +1,50 @@
+﻿/*====================================
+
+  ======================================*/
+
+(function ($) {
+    "use strict";
+    var mainAppNew = {
+
+        main_fun: function () {
+
+            $('#switch-panel').click(function () {
+                if ($(this).hasClass('show-panel')) {
+                    $('.switcher').css({ 'left': '-50px' });
+                    $('#switch-panel').removeClass('show-panel');
+                    $('#switch-panel').addClass('hide-panel');
+                } else if ($(this).hasClass('hide-panel')) {
+                    $('.switcher').css({ 'left': 0 });
+                    $('#switch-panel').removeClass('hide-panel');
+                    $('#switch-panel').addClass('show-panel');
+                }
+            });
+
+            $('#pink').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/pink.css');
+            });
+            $('#green').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/green.css');
+            });
+            $('#blue').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/blue.css');
+            });
+            $('#brown').click(function () {
+                $('#mainCSS').attr('href', 'assets/css/themes/brown.css');
+            });
+          
+        },
+
+        initialization: function () {
+            mainAppNew.main_fun();
+
+        }
+
+    };
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainAppNew.main_fun();
+    });
+
+}(jQuery));
